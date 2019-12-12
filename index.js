83fac43f32ef4c0b4a22e2164a3632dc34af521d
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-11 21:13:03 -0500

    pushing

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express")
 
-//could do this instead of -r dotenv/config in the package.json
-//but since .env ins't uploaded to the repo, it's safer to not try
-// to look for the file in production.
-// const dontenv = require('dotenv')
-
-// dontenv.config()
+// Could do this instead of -r dotenv/config in the package.json
+// but since .env isn't uploaded to the repo, it's safer to not try
+// to look for the file in production.]
+//
+// const dotenv = require("dotenv")
+// 
+// dotenv.config()
 
 const app = express()
-const host = process.env.HOST || "127.0.0.1"
+const host = process.env.HOST || "0.0.0.0"
 const port = process.env.PORT || 8080
-
 
 app.use((req, res, next) => {
 	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
@@ -20,11 +20,11 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
 	res.json({
 		message: "Welcome to our API",
-		// this value is now extracted into out enviorment variable,
-		// so we can keep it out of or code and our repository
-		cohort: process.env.LAMDA_COHORT,
-		secret: process.env.SUPER_SECRET_API_KEY
-		
+		cohort: process.env.LAMBDA_COHORT,
+		// this value is now extracted into our environment variable,
+		// so we can keep it out of our code and our repository
+		// secret: "hfaiwoehfo94032u4hnkljhbf"
+		secret: process.env.SUPER_SECRET_API_KEY,
 	})
 })
 
