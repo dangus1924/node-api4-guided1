48d5933c01683eb8134e535ee762b6f030d4b032
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-11 20:47:40 -0500

    getting ready to deploy on app

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express")
+// const dontenv = require('dotenv')
+
+// dontenv.config()
 
 const app = express()
-const host = "0.0.0.0"
+const host = "127.0.0.1"
 const port = 8080
 
 app.use((req, res, next) => {
@@ -12,6 +15,11 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
 	res.json({
 		message: "Welcome to our API",
+		// this value is now extracted into out enviorment variable,
+		// so we can keep it out of or code and our repository
+		cohort: process.env.LAMDA_COHORT,
+		secret: process.env.SUPER_SECRET_API_KEY
+		
 	})
 })
 
