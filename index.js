d5ead033ed1689964f30ff3cf2160cbb5152ebd4
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-11 21:10:41 -0500

    push

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express()
 const host = process.env.HOST || "127.0.0.1"
 const port = process.env.PORT || 8080
 
+
 app.use((req, res, next) => {
 	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
 	next()
