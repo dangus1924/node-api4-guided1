af98be35889462e5021761c3e633f765c6ae30f9
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-11 21:05:32 -0500

    Add start script and host/port env vars

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express")
+
+//could do this instead of -r dotenv/config in the package.json
+//but since .env ins't uploaded to the repo, it's safer to not try
+// to look for the file in production.
 // const dontenv = require('dotenv')
 
 // dontenv.config()
 
 const app = express()
-const host = "127.0.0.1"
-const port = 8080
+const host = process.env.HOST || "127.0.0.1"
+const port = process.env.PORT || 8080
 
 app.use((req, res, next) => {
 	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
